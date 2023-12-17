01d6a79b4d48b9d7d14818a9fcf7af7072f556d9
desyolanda <desy.yolanda@outlook.com>
2023-12-17 02:37:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,43 @@
 //     document.querySelectorAll('img').style.visibility = 'visible';
 // }
 
+//Intro
+$(function() {
+
+ // apply spans
+   $("h2").lettering();
+
+   // hack to get animations to run again
+   $(".redraw").click(function() {
+     var el = $(this),
+        prev = el.prev(),
+        newone = prev.clone();
+     el.before(newone);
+     $("." + prev.attr("class") + ":first").remove();
+   });
+
+ var text = $("#intro"),
+   numLetters = text.find("span").length;
+
+ function randomBlurize() {
+ text.find("span:nth-child(" + (Math.floor(Math.random()*numLetters)+1) + ")")
+     .animate({
+       'textShadowBlur': Math.floor(Math.random()*25)+4,
+       'textShadowColor': 'rgba(255,101,89,' + (Math.floor(Math.random()*200)+55) + ')'
+     });
+ // Call itself recurssively
+ setTimeout(randomBlurize, 100);
+ } // Call once
+ randomBlurize();
+
+});
+
+////////////////////////////////////Skills
+$(".bar").each(function(){
+  $(this).find(".bar-inner").animate({
+    width: $(this).attr("data-width")
+  },2000)
+});
 
 // Tab Contents
 function openCity(evt, cityName) {
